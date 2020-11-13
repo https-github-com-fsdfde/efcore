b7ae246da5bd740a89f414421db9eebcf4ccaf32
Brice Lambson <brice@bricelam.net>
2020-11-13 12:23:05 -0800

    Suppress Arcade's aggressive warnAsError injection

--- restore.cmd
+++ restore.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& '%~dp0eng\common\build.ps1' -restore %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& '%~dp0eng\common\build.ps1' -warnAsError $false -restore %*"
 exit /b %ErrorLevel%
